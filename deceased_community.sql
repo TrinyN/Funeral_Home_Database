c03b461cba3dd724f9f261daf1ab653d9cd5c6b4
escott556 <123144924+escott556@users.noreply.github.com>
2023-04-22 16:16:13 -0400

    Create deceased_community.sql

---
+++ deceased_community.sql
@@ -0,0 +1,20 @@
+/* SQL queries to create the multi-value community table for the deceased, and view its current state */
+
+/* start */
+
+/* Create Schema */
+CREATE TABLE `deceased_community` (
+  `SSN` int NOT NULL,
+  `armedForces` varchar(45) DEFAULT NULL,
+  `occupation` varchar(45) DEFAULT NULL,
+  `business` varchar(45) DEFAULT NULL,
+  `education` varchar(45) DEFAULT NULL,
+  `church` varchar(45) DEFAULT NULL,
+  `organization` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`SSN`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+/* View Table */
+SELECT * FROM funeral_home.deceased_community;
+
+/* end */
